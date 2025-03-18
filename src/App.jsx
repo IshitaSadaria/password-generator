5c0f19d753271e65ef28cb3ad1da77b3988f46a6
IshitaSadaria <ishitasadaria84308@gmail.com>
2025-03-18 06:13:13 +0530

    logic for password generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,106 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useEffect, useState, useRef } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8);
+  const [password, setPassword] = useState("");
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+
+  // useRef hook
+
+  const passwordRef = useRef(null); 
+  //useRef is used to take refrence o the any element do the manipulation of that element
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    // passwordRef.current?.setSelectionRange(0,3);
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+    for (let i = 0; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]); 
+  // this array is uesd for memozation, it is
+  // added where the dependencies are there and possible use of the function
+  // and do not compare the array of the useCallback with useEffect array, how to optimise the code
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
+
+  // useEffect array is used to run the function when the dependencies are changed
 
   return (
-   <>
-   <h1 className='text-4xl text-center pt-3 text-white'>Password Generator</h1>
-   </>
-  )
+    <>
+      <h1 className="text-4xl text-center pt-3 text-white">
+        Password Generator
+      </h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-800">
+        <h1 className="text-white text-center my-3 pt-3">Password Generator</h1>
+        <div className="flex shadow ng-white rounded-lg overflow-hidden pb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3 bg-white"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button 
+            onClick={copyPasswordToClipboard}
+            className=" outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+            Copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => setLength(e.target.value)}
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="number-Input"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="char-Input"
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="charInput">Characters</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
